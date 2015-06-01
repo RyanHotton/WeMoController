1eebbd840dadd50a6c341c987538291a456de1ed
Ryan Hotton <ryanhotton@gmail.com>
2015-06-01 13:09:09 -0400

    Implement Class Structure
    Class WeMo has been implemented.
    Removed most code from Form1.cs and placed the WeMo related functions
    into WeMo.cs.

--- WeMo Switch/WeMo Switch/Form1.cs
+++ WeMo Switch/WeMo Switch/Form1.cs
@@ -12,7 +12,7 @@ using System.Net;
 using System.Net.Mail;
 using System.IO;
 using System.Xml;
-
+using WeMo_Switch.Classes;
 
 
 
@@ -20,23 +20,17 @@ namespace WeMo_Switch
 {
     public partial class Form1 : Form
     {
-        const string COMMAND_OFF = @"<?xml version=""1.0"" encoding=""utf-8""?><s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/"" s:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/""><s:Body><u:SetBinaryState xmlns:u=""urn:Belkin:service:basicevent:1""><BinaryState>0</BinaryState></u:SetBinaryState></s:Body></s:Envelope>";
-        const string COMMAND_ON = @"<?xml version=""1.0"" encoding=""utf-8""?><s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/"" s:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/""><s:Body><u:SetBinaryState xmlns:u=""urn:Belkin:service:basicevent:1""><BinaryState>1</BinaryState></u:SetBinaryState></s:Body></s:Envelope>";
-        const string COMMAND_GET = @"<?xml version=""1.0"" encoding=""utf-8""?><s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/"" s:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/""><s:Body><u:GetBinaryState xmlns:u=""urn:Belkin:service:basicevent:1""></u:GetBinaryState></s:Body></s:Envelope>";
-        private string WeMo_Status;
         private Timer Timer_Status;
-        private string port = "49153";              // typical port
-        private string baseUrl = "192.168.2.28";    // your WeMo IP
+        private WeMo myWeMo = new WeMo();
 
         public Form1(string ipAddr)
         {
             InitializeComponent(); ;
-            baseUrl = ipAddr;
-            Console.WriteLine(baseUrl + ":" + port + "/icon.jpg");
-            getWeMoIcon();
+            myWeMo.setBaseURL(ipAddr);
             try
             {
-                lblStatus.Text = getStatus();
+                myWeMo.getWeMoIcon(ref picIcon);
+                lblStatus.Text = myWeMo.getStatus(ref btnOff, ref btnOn);
                 InitTimer();
             }
             catch
@@ -47,139 +41,16 @@ namespace WeMo_Switch
            
         }
 
-        private void getWeMoIcon()
-        {
-            string iconURL = "http://" + baseUrl + ":" + port + "/icon.jpg";
-            var request = WebRequest.Create(iconURL);
-
-            using (var response = request.GetResponse())
-            using (var stream = response.GetResponseStream())
-            {
-                picIcon.Image = Bitmap.FromStream(stream);
-            }
-        }
-
-        private string getStatus()
-        {
-            //
-            //  Pull presentation URL from device and extract IP and PORT
-            //
-
-            //
-            //  Build new target URL including the basicevent1 path
-            //
-            string targetUrl = "http://" + baseUrl + ":" + port + "/upnp/control/basicevent1";
-
-            //
-            //  Create the packet and payload to send to the endpoint to get the switch to process the command
-            //
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(targetUrl);
-            request.Method = "POST";
-            request.Headers.Add("SOAPAction", "\"urn:Belkin:service:basicevent:1#GetBinaryState\"");
-            request.ContentType = @"text/xml; charset=""utf-8""";
-            request.KeepAlive = false;
-            Byte[] bytes = UTF8Encoding.ASCII.GetBytes(COMMAND_GET);
-            request.ContentLength = bytes.Length;
-            using (Stream stream = request.GetRequestStream())
-            {
-                stream.Write(bytes, 0, bytes.Length);
-                stream.Close();
-                using (var response = (HttpWebResponse)request.GetResponse())
-                {
-                    using (var reader = new StreamReader(response.GetResponseStream()))
-                    {
-                        string result = reader.ReadToEnd(); // do something fun...
-                        return getStatusName(getStatusFromXML(result));
-                    }
-                }
-                
-            }
-
-        }
-
         private void btnOn_Click(object sender, EventArgs e)
         {
-            SendCommand(COMMAND_ON);
-            lblStatus.Text = getStatus();
-        }
-
-        private void SendCommand(string command)
-        {
-            //
-            //  Pull presentation URL from device and extract IP and PORT
-            //
-
-            //
-            //  Build new target URL including the basicevent1 path
-            //
-            string targetUrl = "http://" + baseUrl + ":" + port + "/upnp/control/basicevent1";
-
-            //
-            //  Create the packet and payload to send to the endpoint to get the switch to process the command
-            //
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(targetUrl);
-            request.Method = "POST";
-            request.Headers.Add("SOAPAction", "\"urn:Belkin:service:basicevent:1#SetBinaryState\"");
-            request.ContentType = @"text/xml; charset=""utf-8""";
-            request.KeepAlive = false;
-            Byte[] bytes = UTF8Encoding.ASCII.GetBytes(command);
-            request.ContentLength = bytes.Length;
-            using (Stream stream = request.GetRequestStream())
-            {
-                stream.Write(bytes, 0, bytes.Length);
-                stream.Close();
-                request.GetResponse();
-            }
-
-            //
-            //  HACK: If we don't abort the result the device holds on to the connection sometimes and prevents other commands from being received
-            //
-            request.Abort();
+            myWeMo.SendCommand(myWeMo.getCommandOn());
+            lblStatus.Text = myWeMo.getStatus(ref btnOff, ref btnOn);
         }
 
         private void btnOff_Click(object sender, EventArgs e)
         {
-            SendCommand(COMMAND_OFF);
-            lblStatus.Text = getStatus();
-        }
-
-        private string getStatusFromXML(string statusXML)
-        {
-            // load xml
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(statusXML);
-
-            //Display all the book titles.
-            XmlNodeList elemList = doc.GetElementsByTagName("BinaryState");
-            //Console.WriteLine(elemList[0].InnerXml);
-            return (string)elemList[0].InnerXml;
- 
-        }
-
-        private string getStatusName(string status)
-        {
-            string name = "";
-
-            btnOff.Enabled = true;
-            btnOn.Enabled = true;
-
-            switch(status)
-            {
-                case "1":
-                    name = "ON";
-                    btnOn.Enabled = false;
-                    break;
-                case "0":
-                    name = "OFF";
-                    btnOff.Enabled = false;
-                    break;
-                default:
-                    name = "ERROR";
-                    break;
-            }
-
-            WeMo_Status = name;
-            return name;
+            myWeMo.SendCommand(myWeMo.getCommandOff());
+            lblStatus.Text = myWeMo.getStatus(ref btnOff, ref btnOn);
         }
 
         public void InitTimer()
@@ -191,9 +62,9 @@ namespace WeMo_Switch
         }
 
         private void Timer_Status_Tick(object sender, EventArgs e)
-        {               
-            lblStatus.Text = getStatus();
-            Console.WriteLine("WeMo Status: " + WeMo_Status);
+        {
+            lblStatus.Text = myWeMo.getStatus(ref btnOff, ref btnOn);
+            Console.WriteLine("WeMo Status: " + myWeMo.getWeMo_Status());
         }
 
         private void Form1_Load(object sender, EventArgs e)
