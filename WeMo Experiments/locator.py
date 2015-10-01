4926cd993a1c66b99daf58cc75453cd97cf633f8
Ryan Hotton <ryanhotton@gmail.com>
2015-09-30 20:02:37 -0400

    added more to the experiment

--- WeMo Experiments/locator.py
+++ WeMo Experiments/locator.py
@@ -1,5 +1,5 @@
 import os
-
+import socket
 # sources:
 # http://cmikavac.net/2011/09/11/how-to-generate-an-ip-range-list-in-python/
 def ipRange(start_ip, end_ip):
@@ -25,14 +25,34 @@ def isValidIP(hostname):
      print hostname, 'is up!'
    else:
      print hostname, 'is down!'  
-   
-# sample usage 
-start_ip = "192.168.1.0"
-end_ip = "192.171.3.25"
-ip_range = ipRange(start_ip, end_ip)
-count = 0
-for ip in ip_range:
-   #print(ip)
-   count += 1
-   #isValidIP(ip)
-print(count)
+
+def generateIPList(ip):    
+   # sample usage 
+   start_ip = ipStartEnd(ip, 0) #"192.168.1.0"
+   end_ip = ipStartEnd(ip, 1) #"192.171.3.25"
+   ip_range = ipRange(start_ip, end_ip)
+   count = 0
+   for ip in ip_range:
+      print(ip)
+      count += 1
+      #isValidIP(ip)
+   print("Total IPs Generated: " + str(count))
+
+def localIP():
+   ip = ([(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+   return ip
+
+def ipStartEnd(ip, type):
+   if type == 0:
+      return ".".join(ip.split('.')[0:-1]) + '.0'
+   else:
+      return ".".join(ip.split('.')[0:-1]) + '.255'
+
+##############  END OF DEF ############## 
+
+ip = localIP()       # gets local ip
+generateIPList(ip)   # generates a range from local ip
+print("Local IP: " + ip)
+
+
+
